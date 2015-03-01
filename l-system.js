248460682526192ba1ef110f7e5481b5b87f80b8
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-03-01 16:21:08 +0000

    Clean up

--- l-system.js
+++ l-system.js
@@ -1,47 +1,45 @@
-const LSystem = function LSystem(axiom, productionRules) {
-    if(!(Symbol.iterator in productionRules)) {
-        throw new Error("LSystem: productionRules (argument 1) must be iterable");
-    }
-
-    productionRules = new Map(productionRules);
-
-    const alphabet = Array.from(productionRules.keys()),
-        constants = [];
-
-    if(alphabet.indexOf(axiom) === -1) {
-        throw new Error("LSystem: axiom (argument 0) has to be in productionRules");
-    }
-
-    for(let [from, to] of productionRules) {
-        if(typeof(to) === "string" || !(Symbol.iterator in to)) {
-            to = [to];
-            productionRules.set(from, to);
+class LSystem {
+    constructor(axiom, productionRules) {
+        if(!(Symbol.iterator in productionRules)) {
+            throw new Error("LSystem: productionRules (argument 1) must be iterable");
         }
 
-        if(to.length === 1 && to[0] === from) {
-            constants.push(to);
+        productionRules = new Map(productionRules);
+
+        const alphabet = Array.from(productionRules.keys()),
+            constants = [];
+
+        if(alphabet.indexOf(axiom) === -1) {
+            throw new Error("LSystem: axiom (argument 0) has to be in productionRules");
         }
 
-        for(let ele of to) {
-            if(alphabet.indexOf(ele) === -1) {
-                throw new Error(`LSystem: unknown value ${ele} in productionRules of ${from}`);
+        for(let [from, to] of productionRules) {
+            if(typeof(to) === "string" || !(Symbol.iterator in to)) {
+                to = [to];
+                productionRules.set(from, to);
+            }
+
+            if(to.length === 1 && to[0] === from) {
+                constants.push(to);
+            }
+
+            for(let ele of to) {
+                if(alphabet.indexOf(ele) === -1) {
+                    throw new Error(`LSystem: unknown value ${ele} in productionRules of ${from}`);
+                }
             }
         }
+
+        Object.assign(this, {
+            "axiom": axiom,
+            "alphabet": alphabet,
+            "constants": constants,
+            "productionRules": productionRules
+        });
+        this.reset();
     }
 
-    Object.assign(this, {
-        "axiom": axiom,
-        "alphabet": alphabet,
-        "constants": constants,
-        "productionRules": productionRules
-    });
-    this.reset();
-};
-
-export default LSystem;
-
-Object.assign(LSystem.prototype, {
-    "step": function() {
+    step() {
         const next = [];
 
         for(let ele of this.current) {
@@ -49,11 +47,13 @@ Object.assign(LSystem.prototype, {
         }
 
         this.current = next;
-    },
-    "reset": function() {
+    }
+
+    reset() {
         this.current = [this.axiom];
-    },
-    "ended": function() {
+    }
+
+    ended() {
         for(let ele of this.current) {
             if(this.constants.indexOf(ele) === -1) {
                 return false;
@@ -61,8 +61,9 @@ Object.assign(LSystem.prototype, {
         }
 
         return true;
-    },
-    "generator": function*() {
+    }
+
+    * generator() {
         while(true) {
             yield this.current;
 
@@ -72,7 +73,10 @@ Object.assign(LSystem.prototype, {
             }
         }
     }
-});
-LSystem.prototype[Symbol.iterator] = function() {
-    return (new LSystem(this.axiom, this.productionRules)).generator();
-};
+
+    [Symbol.iterator]() {
+        return (new LSystem(this.axiom, this.productionRules)).generator();
+    }
+}
+
+export default LSystem;
