fd5329de6dba521b445e7ad9b808336fdef5006a
Nick Heiner <nick.heiner@opower.com>
2015-08-18 15:20:21 -0400

    comment

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ require('babel/register')
 gulp.task('build', function () {
   return gulp.src('{vendor/**/*.js,' + main + ',cli.js,lib/**/*.js}')
     .pipe(sourcemaps.init())
+    // Ideally, this would use Webpack or Browserify, but for now we will just concat the files together.
       .pipe(concat('l-system.js'))
       .pipe(babel())
       .pipe(replace(/#!\/usr\/bin\/env babel-node/, '#!/usr/bin/env node'))
